6126654b3dff6aada7274cd43eaa3835aac06a97
mohdfarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-05-28 12:13:21 +0200

    Add transfer function

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreen
 import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
 import vtkImageMarchingCubes from '@kitware/vtk.js/Filters/General/ImageMarchingCubes';
 import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps';
 
 import { vec3, quat, mat4 } from 'gl-matrix';
 import vtkWidgetManager from '@kitware/vtk.js/Widgets/Core/WidgetManager';
@@ -26,87 +27,95 @@ import vtkPiecewiseFunction from '@kitware/vtk.js/Common/DataModel/PiecewiseFunc
 import vtkVolume from '@kitware/vtk.js/Rendering/Core/Volume';
 import vtkVolumeMapper from '@kitware/vtk.js/Rendering/Core/VolumeMapper';
 import vtkPlane from '@kitware/vtk.js/Common/DataModel/Plane';
-
-// Force the loading of HttpDataAccessHelper to support gzip decompression
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import Widgets3D from '@kitware/vtk.js/Widgets/Widgets3D';
+import vtkPiecewiseGaussianWidget from '@kitware/vtk.js/Interaction/Widgets/PiecewiseGaussianWidget';
 
 const __BASE_PATH__ = 'https://kitware.github.io/vtk-js/'
 const controlPanel = `
 <table>
+  <tr>
+    <td><label>Load</label></td>
+    <td>
+    <div class="${style.bigFileDrop}"/><input type="file" accept=".vti" style="display: none;"/>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <input id="rendering" type="radio" name="type" value="1"><label>Rendering</label>
+    </td>
+    <td>
+      <input id="rayCasting" type="radio" name="type" value="1"><label>Ray Casting</label>
+    </td>
+  </tr>
+  <tr id="layoutRendering">
+    <td><label>Iso value</label></td>
+    <td><input class='isoValue' type="range" min="0.0" max="1.0" step="0.05" value="0.0" /></td>
+  </tr>
+  <tbody id="layoutRayCasting">
     <tr>
-      <td>
-        <input id="rendering" type="radio" name="type" value="1"><label>Rendering</label>
-      </td>
-      <td>
-        <input id="rayCasting" type="radio" name="type" value="1"><label>Ray Casting</label>
+      <td><label>pickable</label></td>
+      <td><input class='flag' data-name="pickable" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>visibility</label></td>
+      <td><input class='flag' data-name="visibility" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>contextVisibility</label></td>
+      <td><input class='flag' data-name="contextVisibility" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>handleVisibility</label></td>
+      <td><input class='flag' data-name="handleVisibility" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>faceHandlesEnabled</label></td>
+      <td><input class='flag' data-name="faceHandlesEnabled" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>edgeHandlesEnabled</label></td>
+      <td><input class='flag' data-name="edgeHandlesEnabled" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td><label>cornerHandlesEnabled</label></td>
+      <td><input class='flag' data-name="cornerHandlesEnabled" type="checkbox" checked /></td>
+    </tr>
+    <tr>
+      <td colspan="2"><div id="ray-transfer-function"></div></td>
+    </tr>
+    <tr>
+      <td colspan="2">
+        <select hidden id="presets-menu">
+          <option selected value="Cool to Warm">Cool to Warm</option>
+          <option value="Cold and Hot">Cold and Hot</option>
+          <option value="Black, Blue and White">Black, Blue and White</option>
+          <option value="X Ray">X Ray</option>
+          <option value="erdc_rainbow_dark">Edrc Rainbow Dark</option>
+        </select>
       </td>
     </tr>
-    <tr id="layoutRendering">
-      <td><label>Iso value</label></td>
-      <td><input class='isoValue' type="range" min="0.0" max="1.0" step="0.05" value="0.0" /></td>
-    </tr>
-    <tbody id="layoutRayCasting">
-      <tr>
-        <td><label>pickable</label></td>
-        <td><input class='flag' data-name="pickable" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>visibility</label></td>
-        <td><input class='flag' data-name="visibility" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>contextVisibility</label></td>
-        <td><input class='flag' data-name="contextVisibility" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>handleVisibility</label></td>
-        <td><input class='flag' data-name="handleVisibility" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>faceHandlesEnabled</label></td>
-        <td><input class='flag' data-name="faceHandlesEnabled" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>edgeHandlesEnabled</label></td>
-        <td><input class='flag' data-name="edgeHandlesEnabled" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td><label>cornerHandlesEnabled</label></td>
-        <td><input class='flag' data-name="cornerHandlesEnabled" type="checkbox" checked /></td>
-      </tr>
-      <tr>
-        <td colspan="2"><div id="ray-transfer-function"></div></td>
-      </tr>
-      <tr>
-        <td colspan="2">
-          <select id="presets-menu">
-            <option selected value="Cool to Warm">Cool to Warm</option>
-            <option value="Cold and Hot">Cold and Hot</option>
-            <option value="Black, Blue and White">Black, Blue and White</option>
-            <option value="X Ray">X Ray</option>
-            <option value="erdc_rainbow_dark">Edrc Rainbow Dark</option>
-          </select>
-        </td>
-      </tr>
-    </tbody>
+  </tbody>
 </table>
-
 `
 
+const rootContainer = document.querySelector(
+  '.vtk-js-example-piecewise-gaussian-widget'
+);
 
+var srcItem = `${__BASE_PATH__}/data/volume/headsq.vti`
 // ----------------------------------------------------------------------------
 // Code setup
 // ----------------------------------------------------------------------------
 
 async function renderHead() {
 
+  document.body.innerHTML = ""
+
   function updateIsoValue(e) {
     const isoValue = Number(e.target.value);
     marchingCube.setContourValue(isoValue);
     renderWindow.render();
   }
-
-  document.body.innerHTML = ""
 
   const fullScreenRenderWindow = vtkFullScreenRenderWindow.newInstance({
     background: [0, 0, 0],
@@ -129,7 +138,9 @@ async function renderHead() {
 
   const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
 marchingCube.setInputConnection(reader.getOutputPort());
-  reader.setUrl(`${__BASE_PATH__}/data/volume/headsq.vti`, { loadData: true })
+
+// ${__BASE_PATH__}/data/volume/headsq.vti
+  reader.setUrl(srcItem, { loadData: true })
   .then(() => {
     const data = reader.getOutputData();
     const dataRange = data.getPointData().getScalars().getRange();
@@ -168,6 +179,13 @@ marchingCube.setInputConnection(reader.getOutputPort());
     rayCastingChest();
   });
 
+  loadFile.onchange = evt => {
+    const [file] = loadFile.files
+    if (file) {
+      srcItem = URL.createObjectURL(file)
+      // reader.setUrl(srcItem);
+    }
+  }
 }
 
 async function rayCastingChest() {
@@ -198,6 +216,30 @@ async function rayCastingChest() {
   overlay.style.pointerEvents = 'none';
   document.querySelector('body').appendChild(overlay);
 
+  const widgetContainer = document.createElement('div');
+  widgetContainer.style.position = 'absolute';
+  widgetContainer.style.top = 'calc(340px + 1em)';
+  widgetContainer.style.left = '5px';
+  widgetContainer.style.background = 'rgba(255, 255, 255, 0.3)';
+  // widgetContainer.style.visibility = "hidden"
+  document.querySelector('body').appendChild(widgetContainer);
+  //actor.setVisibility(false);
+  //widget.set({"visibility":false});
+
+  const labelContainer = document.createElement('div');
+  labelContainer.style.position = 'absolute';
+  labelContainer.style.top = '5px';
+  labelContainer.style.left = '5px';
+  labelContainer.style.width = '100%';
+  labelContainer.style.color = 'white';
+  labelContainer.style.textAlign = 'center';
+  labelContainer.style.userSelect = 'none';
+  labelContainer.style.cursor = 'pointer';
+  document.querySelector('body').appendChild(labelContainer);
+  let presetIndex = 1;
+  const globalDataRange = [0, 255];
+  const lookupTable = vtkColorTransferFunction.newInstance();
+  
   // ----------------------------------------------------------------------------
   // Widget manager
   // ----------------------------------------------------------------------------
@@ -306,9 +348,72 @@ async function rayCastingChest() {
     ];
   }
 
+  const widget1 = vtkPiecewiseGaussianWidget.newInstance({
+    numberOfBins: 256,
+    size: [400, 150],
+  });
+
+  widget1.updateStyle({
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    histogramColor: 'rgba(100, 100, 100, 0.5)',
+    strokeColor: 'rgb(0, 0, 0)',
+    activeColor: 'rgb(255, 255, 255)',
+    handleColor: 'rgb(50, 150, 50)',
+    buttonDisableFillColor: 'rgba(255, 255, 255, 0.5)',
+    buttonDisableStrokeColor: 'rgba(0, 0, 0, 0.5)',
+    buttonStrokeColor: 'rgba(0, 0, 0, 1)',
+    buttonFillColor: 'rgba(255, 255, 255, 1)',
+    strokeWidth: 2,
+    activeStrokeWidth: 3,
+    buttonStrokeWidth: 1.5,
+    handleWidth: 3,
+    iconSize: 20, // Can be 0 if you want to remove buttons (dblClick for (+) / rightClick for (-))
+    padding: 10,
+  });
+
+
+  fullScreenRenderWindow.setResizeCallback(({ width, height }) => {
+    widget1.setSize(Math.min(450, width - 10), 150);
+  });
+
+  const piecewiseFunction = vtkPiecewiseFunction.newInstance();
+
+  // 
   reader.setUrl(`${__BASE_PATH__}/data/volume/LIDC2.vti`).then(() => {
-    reader.loadData().then(() => {
+    
+      reader.loadData().then(() => {
       const image = reader.getOutputData();
+      const dataArray = image.getPointData().getScalars();
+      const dataRange = dataArray.getRange();
+      globalDataRange[0] = dataRange[0];
+      globalDataRange[1] = dataRange[1];
+
+      function changePreset(delta = 1) {
+        presetIndex =
+          (presetIndex + delta + vtkColorMaps.rgbPresetNames.length) %
+          vtkColorMaps.rgbPresetNames.length;
+        lookupTable.applyColorMap(
+          vtkColorMaps.getPresetByName(vtkColorMaps.rgbPresetNames[presetIndex])
+        );
+        lookupTable.setMappingRange(...globalDataRange);
+        lookupTable.updateRange();
+        labelContainer.innerHTML = vtkColorMaps.rgbPresetNames[presetIndex];
+      }
+      let intervalID = null;
+    
+
+      // Automatic switch to next preset every 5s
+        if (!rootContainer) {
+          intervalID = setInterval(changePreset, 5000);
+        }
+        widget1.setDataArray(dataArray.getData());
+        widget1.applyOpacity(piecewiseFunction);
+    
+        widget1.setColorTransferFunction(lookupTable);
+        lookupTable.onModified(() => {
+          widget1.render();
+          renderWindow.render();
+        });
 
       // update crop widget
       widget.copyImageDataDescription(image);
@@ -333,7 +438,7 @@ async function rayCastingChest() {
   // -----------------------------------------------------------
   // UI control handling
   // -----------------------------------------------------------
-
+  
   function updateFlag(e) {
     const value = !!e.target.checked;
     const name = e.currentTarget.dataset.name;
@@ -353,6 +458,35 @@ async function rayCastingChest() {
     buttons[i].addEventListener('click', widgetRegistration);
   }
 
+  actor.getProperty().setRGBTransferFunction(0, lookupTable);
+  actor.getProperty().setScalarOpacity(0, piecewiseFunction);
+  actor.getProperty().setInterpolationTypeToFastLinear();
+
+  // ----------------------------------------------------------------------------
+  // Default setting Piecewise function widget
+  // ----------------------------------------------------------------------------
+
+  widget1.addGaussian(0.425, 0.5, 0.2, 0.3, 0.2);
+  widget1.addGaussian(0.75, 1, 0.3, 0, 0);
+
+  widget1.setContainer(widgetContainer);
+  widget1.bindMouseListeners();
+
+  widget1.onAnimation((start) => {
+    if (start) {
+      renderWindow.getInteractor().requestAnimation(widget1);
+    } else {
+      renderWindow.getInteractor().cancelAnimation(widget1);
+    }
+  });
+
+  widget1.onOpacityChange(() => {
+    widget1.applyOpacity(piecewiseFunction);
+    if (!renderWindow.getInteractor().isAnimating()) {
+      renderWindow.render();
+    }
+  });
+
   document.getElementById('layoutRendering').style.visibility = 'collapse';
   document.getElementById('layoutRayCasting').style.visibility = 'visible';
   document.getElementById('ray-transfer-function').style.visibility = 'visible';
@@ -366,6 +500,18 @@ async function rayCastingChest() {
   document.getElementById('rayCasting').addEventListener('change', function () {
     rayCastingChest();
   });
+
+  loadFile.onchange = evt => {
+    const [file] = loadFile.files
+    if (file) {
+      srcItem = `${__BASE_PATH__}/data/volume/${file.name}`
+      console.log(srcItem)
+      // reader.setUrl(srcItem);
+      // renderWindow.render();
+    }
+  }
+
+  global.widget1 = widget1;
 }
 
 async function start(){
@@ -374,3 +520,4 @@ async function start(){
 }
 
 start();
+
